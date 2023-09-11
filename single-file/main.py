1d47029cde25ada4f3954d9e088ae052533f81e9
Rohit Rajesh <rajesh.rohit04@gmail.com>
2023-09-11 19:37:48 +0530

    Update and rename main.py to single-file/main.py

--- single-file/main.py
+++ single-file/main.py
@@ -11,7 +11,7 @@ from utils import df_info
 
 
 df = pd.DataFrame()
-csv_path = "D:\Desktop\Projects\whatsapp\WhatsApp Chat with Harikesha.csv"
+csv_path = "" # change file path
 sys_msg = f"""You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
 
 def init_page():
